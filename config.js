d7f890ea15efafce3017e95a8e956dd5ca5bb7ff
maleeshaMD <maleeshahansajith2004@gmail.com>
2024-09-26 17:47:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id ",
+SESSION_ID: process.env.SESSION_ID || "1qd1xAQC#jLx-H8PpvtjQHfGYTzgI3hr0q-JAt4EStKWjRWJwMCo ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-SUDO_NB: process.env.SUDO_NB || "94704227534",
+SUDO_NB: process.env.SUDO_NB || "94742186543",
 OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
 BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
